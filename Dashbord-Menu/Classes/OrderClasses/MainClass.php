690a5e12da14dd2bb79491f3e9f854124f9dcf72
AIT-MOHAMED Oussama <sadosami297@gmail.com>
2024-11-08 18:18:25 +0100

    Procedure of orders list is done

---
+++ Dashbord-Menu/Classes/OrderClasses/MainClass.php
@@ -0,0 +1,28 @@
+<?php
+
+class clsOrders
+{
+
+    private static function Conncect()
+    {
+        include_once 'C:\xampp\desktop\htdocs\Resto_Project\Dashbord-Menu\Classes\DbhConnection\Dbh.php';
+        return Dbh::connect();
+    }
+
+
+    public static function LstOrders()
+    {
+        
+    }
+
+    public static function AcceptOrder()
+    {
+        
+    }
+    
+    public static function CancelOrder()
+    {
+        
+    }
+
+}
---
+++ DataBase/Orders/ListeOrder.sql
@@ -0,0 +1,27 @@
+
+DELIMITER //
+
+CREATE PROCEDURE Orders_Liste()
+BEGIN
+    SELECT 
+        users.last_name,
+        menu_items.name AS DishName,
+        orders.order_date, 
+        order_items.price, 
+        order_items.quantity,  
+        order_items.price * order_items.quantity AS 'Total_Amount',
+        orders.delivery_method, 
+        orders.delivery_address,
+        orders.status
+    FROM 
+        users
+    INNER JOIN orders ON orders.user_id = users.user_id
+    INNER JOIN order_items ON order_items.order_id = orders.order_id
+    INNER JOIN menu_items ON menu_items.item_id = order_items.item_id
+    ORDER BY 
+        users.last_name ASC,    -- Sort by last name in ascending order
+        orders.order_date DESC; -- Sort by order date in descending order (you can change it to ASC for ascending)
+END //
+
+DELIMITER ;
+
