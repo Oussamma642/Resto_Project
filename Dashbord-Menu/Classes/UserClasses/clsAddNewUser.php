defcd2f4984d07d3b891150a74e65c021e2e13c2
AIT-MOHAMED Oussama <sadosami297@gmail.com>
2024-11-15 16:31:35 +0100

    The permissions structure is well done

--- Dashbord-Menu/Classes/UserClasses/clsAddNewUser.php
+++ Dashbord-Menu/Classes/UserClasses/clsAddNewUser.php
@@ -4,35 +4,38 @@ include_once 'clsUser.php';
 
 class clsAddNewUser
 {
-    // private static $_permissions = 0;
+    private static $_permissions = 0;
 
-    // private static function handlePermissions($perms)
-    // {
-    //     foreach($perms as $p) {
-    //         self::$_permissions += $p; 
-    //     }
-    // }
+    private static function handlePermissions($perms)
+    {
+        foreach($perms as $p) {
+            self::$_permissions += $p; 
+        }
+    }
 
     public static function AddNewUser()
     {
         if (isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['phoneNumber']) && isset($_POST['permissions'])) {
 
-            echo "Yes";
 
-            // $perms = $_POST['permissions'];
-            // if (in_array(-1, $perms)) {
-            //     echo 'All permissions selected<br><br>';
-            //     return; 
-            // } 
-            // else {
-            //     self::handlePermissions($perms);
-            //     echo 'Permissions result: ' . self::$_permissions;
-            // }
+            $perms = $_POST['permissions'];
+            
+            if (in_array(-1, $perms)) 
+            {
+                echo "<script>console.log('All permissions selected')</script>";
+                return; 
+            } 
+            else {
+                self::handlePermissions($perms);
+                $p = self::$_permissions;
+                echo "<script>console.log('The selected Permissions are: $p')</script>";
+            
+            }
         } 
         else {
             
-            echo "No";
-            // header("location:../Dashbord-Menu/Users.php");
+            
+            header("location:../Dashbord-Menu/Users.php");
         }
     }
 }
