baa849b4e8c055bf0f4ce578e763623ed559aaa4
AIT-MOHAMED Oussama <sadosami297@gmail.com>
2024-11-15 12:25:05 +0100

    Users Insertion Procedure done

--- DataBase/Creation.sql
+++ DataBase/Creation.sql
@@ -10,6 +10,7 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'client') NOT NULL,
     phone_number VARCHAR(20),
+    permissions int,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
---
+++ DataBase/Users/InsertProc.sql
@@ -0,0 +1,17 @@
+delimiter //
+
+CREATE PROCEDURE add_new_user(
+    IN fname VARCHAR(255),
+    IN lname VARCHAR(255),
+    IN em VARCHAR(255),
+    IN pswd VARCHAR(255),
+    IN rle VARCHAR(255),
+    IN phone VARCHAR(255),
+    IN perm INT
+)
+BEGIN
+    INSERT INTO users (first_name, last_name, email, password, role, phone_number, permissions)
+    VALUES (fname, lname, em, pswd, rle, phone, perm);
+END //
+
+delimiter ;
