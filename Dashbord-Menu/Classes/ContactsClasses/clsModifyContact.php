ff87fec8d0f2cbf6584cd13da5fd2e705a18b162
AIT-MOHAMED Oussama <sadosami297@gmail.com>
2024-11-18 16:14:15 +0100

    Transfer the handling email from user classe to contact class

--- Dashbord-Menu/Classes/ContactsClasses/clsModifyContact.php
+++ Dashbord-Menu/Classes/ContactsClasses/clsModifyContact.php
@@ -6,22 +6,9 @@ class clsModifyContact
 {
     public static function ModifyContact()
     {
-        // Start the session to access session variables
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
-
-        // Check if the current user is set in the session
-        if (!isset($_SESSION['currUser'])) {
-            $_SESSION['ContactStatusMessage'] = "User is not logged in.";
-            header("location:../Dashbord-Menu/Contact.php");
-            exit();
-        }
-
-        $currUser = $_SESSION['currUser']; // Retrieve the current user object from the session
-
         if (
-            isset($_POST['contactId']) && 
+            isset($_POST['contactId']) &&
+            isset($_POST['fullname']) && 
             isset($_POST['email']) && 
             isset($_POST['subject']) &&
             isset($_POST['reponse']) &&
@@ -30,13 +17,15 @@ class clsModifyContact
             $mod = clsContact::ModifyContact($_POST['contactId'], $_POST['reponse'], 'resolved');
 
             if ($mod) {
-                $currUser->SendMail($_POST['email'], $_POST['subject'], $_POST['reponse']);
+                clsContact::SendMail($_POST['email'], $_POST['fullname'] , $_POST['subject'], $_POST['reponse']);
             }
             
-            // $_SESSION['ContactStatusMessage'] = ($mod) ? 
-            // "The response has been successfully sent" :
-            // "Failed to send the response!!";
-        } else {
+            $_SESSION['ContactStatusMessage'] = ($mod) ? 
+            "The response has been successfully sent" :
+            "Failed to send the response!!";
+            
+        } 
+        else {
             $_SESSION['ContactStatusMessage'] = "Missing field, check your form inputs!!";
         }
         header("location:../Dashbord-Menu/Contact.php");
