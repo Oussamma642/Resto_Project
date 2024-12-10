868d4f2901e937863177971b5eec7aecb619b982
AIT-MOHAMED Oussama <sadosami297@gmail.com>
2024-12-10 13:43:06 +0100

    Lst Commnts

---
+++ Dashbord-Menu/Classes/CommentsClasses/clsComment.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once 'C:\xampp\desktop\htdocs\Resto_Project\Dashbord-Menu\Classes\ContactsClasses\clsContact.php';
+
+
+class clsComment 
+{
+
+    protected $Contact;
+    
+    private static function Conncect()
+    {
+        include_once 'C:\xampp\desktop\htdocs\Resto_Project\Dashbord-Menu\Classes\DbhConnection\Dbh.php';
+        return Dbh::connect();
+    }
+
+    public static function ListComments()
+    { 
+        // Conncet with DB
+        $conn = self::Conncect();
+
+        // Prepare and execute the statement
+        $stmt = $conn->prepare("CALL Comments_List()");
+        $stmt->execute();
+
+        // Fetch all reservations as an associative array
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+
+}
